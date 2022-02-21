bb6298c77f53643c59a71e6759c8deb211c8ebf0
Çağlar Yalçın <iamcaglar@pm.me>
2022-02-22 00:43:21 +0300

    Add files via upload

---
+++ scripts/dark.sh
@@ -0,0 +1,4 @@
+gsettings set org.cinnamon.theme name Mint-Y-Dark
+gsettings set org.cinnamon.desktop.interface gtk-theme Mint-Y-Dark
+gsettings set org.cinnamon.desktop.interface icon-theme Mint-Y-Dark
+gsettings set org.cinnamon.desktop.wm.preferences theme Mint-Y
